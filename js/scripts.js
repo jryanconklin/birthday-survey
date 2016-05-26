36cd9832842066e994d7ab6585edb6522629d3f8
Lauren Burnett and J. Ryan Conklin <jryanconklin@gmail.com>
2016-05-25 21:06:14 -0700

    Fix variable bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 
 $(document).ready(function() {
   $("#data form").submit(function(event) {
-    var cupcake = $("#cupcake").val();
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    var favoriteColor = $("#color").val();
-    var income = $("#income").val();
-    var dob = $("#birthdate").val();
+    var cupcakeInput = $("#cupcake").val();
+    var flavorInput = $("input:radio[name=flavor]:checked").val();
+    var favoriteColorInput = $("#color").val();
+    var incomeInput = $("#income").val();
+    var dobInput = $("#birthdate").val();
 
     $(".cupcake").text(cupcakeInput);
     $(".flavor").text(flavorInput);
     $(".favoriteColor").text(favoriteColorInput);
     $(".income").text(incomeInput);
     $(".dob").text(dobInput);
-
-    $("#data").hide();
-    $("#results").show();
 
     event.preventDefault();
   });
