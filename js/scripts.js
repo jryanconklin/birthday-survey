028a6990f426287263140a1592bc9c23e60af864
Lauren Burnett and J. Ryan Conklin <lauren.e.burnett@gmail.com>
2016-05-25 20:02:50 -0700

    Add birthday cake color picker

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $("#data form").submit(function(event) {
     var cupcake = $("#cupcake").val();
     var flavor = $("input:radio[name=flavor]:checked").val();
-
+    var favoriteColor = $("#color").val();
     $("#data").hide();
     $("#results").show();
 
