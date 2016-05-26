0d6cb1620b09aa0121db370d6d0fd8054ada393c
Lauren Burnett and J. Ryan Conklin <jryanconklin@gmail.com>
2016-05-25 19:56:42 -0700

    add ice cream radio button

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   $("#data form").submit(function(event) {
     var cupcake = $("#cupcake").val();
-
+    var flavor = $("input:radio[name=flavor]:checked").val();
 
     $("#data").hide();
     $("#results").show();
